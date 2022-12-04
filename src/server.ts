97c85934776fa64a6eb8fc7ae371de8c31225f4f
Maulana Adil <maulanaadil.latif@gmail.com>
2022-12-04 14:00:19 +0700

    feat: add debugger

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,14 @@ require("module-alias/register");
 import router from "./routes";
 import helmet from "helmet";
 import compression from "compression";
+import http from "http";
+var debug = require("debug")("maulnad-porto-be:server");
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -43,6 +46,36 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
+server.on("listening", onListening);
+server.on("error", onError);
+
+function onError(error: any) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
+  debug("Listening on " + bind);
+}
